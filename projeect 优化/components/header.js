5dd33d116024309a6660bc734e98980c898da0cd
paopao97 <13724716636@163.com>
2020-06-05 16:22:11 +0800

    第一次提交

---
+++ projeect 优化/components/header.js
@@ -0,0 +1,13 @@
+(function(Vue){
+     /* 
+    1. 实现私有变量
+    2. 延长变量的生命周期
+    */
+    Vue.component("app-header",{
+        template:`
+            <div class="app-header">
+                <h1>代办事项</h1>
+            </div>
+        `
+    })
+})(Vue)
